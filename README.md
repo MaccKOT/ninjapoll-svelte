4a3a7d218f2d7357f053c0edb02dac87691e0a7a
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-08-12 07:09:40 +0400

    Inline events

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # [Svelte crash course by The Net Ninja](https://www.youtube.com/watch?v=lnpdn2rE2N8)
 
-In case of using not **yarn** package manager, change params in `rollup.config.js` at line 20.
+In case of using not **yarn** package manager, change params in `rollup.config.js` at the line 20.
 
 ```javascript
   return {
--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,13 @@
     { name: "mario", beltColour: "orange", age: 45, id: 2 },
     { name: "luigi", beltColour: "brown", age: 35, id: 3 }
   ];
+
+  const handleClick = id => {
+    //delete person from people array
+    //оставим в массиве элементы, id которых не равен тому, что надо удалять
+    //важно переназначить переменную, а не просто применить на ней метод filter! Только переназначение активирует реактивную связь
+    people = people.filter(person => person.id != id);
+  };
 </script>
 
 <style>
@@ -27,8 +34,9 @@
     <div>
       <h4>{person.name}</h4>
       <p>{person.age} years old, {person.beltColour} belt</p>
+      <!-- мы не можем использовать handleClick(person.id), потому что это сразу исполнит функцию, поэтому надо вызовать через arrow-функцию -->
+      <button on:click={() => handleClick(person.id)}>delete</button>
     </div>
-    <!-- можно использовать директиву else для проверки на пустой массив -->
   {:else}
     <p>There are no people to show...</p>
   {/each}
