51b76ec5a12f4d8c400ba3f366b20238051de5ee
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-08-13 10:29:57 +0400

    Header & Footer Components

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,22 @@
 <script>
-  let name = 'Svelte';
+  import Header from './components/Header.svelte';
+  import Footer from './components/Footer.svelte';
 </script>
 
 <style>
   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+    max-width: 960px;
+    margin: 40px auto;
   }
 </style>
 
+<Header />
 <main>
-  <h1>Hello {name}!</h1>
   <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto mollitia
+    libero rerum dolore aperiam voluptate, consequatur ea eligendi debitis
+    necessitatibus tempore deserunt! Quasi esse sed numquam iure quas incidunt
+    doloremque.
   </p>
 </main>
+<Footer />
---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<style>
+  header {
+    background-color: #f7f7f7;
+    padding: 10px;
+  }
+  h1 {
+    margin: 0;
+    text-align: center;
+  }
+  img {
+    max-width: 180px;
+  }
+</style>
+
+<!-- no scripts needed here, just html template -->
+
+<header>
+  <h1>
+    <!-- путь от папки в public -->
+    <img src="/img/logo.svg" alt="Poll Ninja Logo" />
+  </h1>
+</header>
