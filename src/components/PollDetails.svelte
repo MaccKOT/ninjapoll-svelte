97943d6b6fd31e3b1209d78b4ea7d71126a2dcb1
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-08-14 08:35:04 +0400

    Vote Bars

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -6,6 +6,8 @@
 
   //reactive value
   $: totalVotes = poll.votesA + poll.votesB;
+  $: percentA = Math.floor((100 / totalVotes) * poll.votesA);
+  $: percentB = 100 - percentA;
 
   //handling votes
   const handleVote = (option, id) => {
@@ -29,6 +31,7 @@
     cursor: pointer;
     margin: 10px auto;
     position: relative;
+    user-select: none;
   }
   .answer:hover {
     opacity: 0.6;
@@ -37,6 +40,20 @@
     display: inline-block;
     padding: 10px 20px;
   }
+
+  .percent {
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+  }
+  .percent-a {
+    border-left: 4px solid #d91b42;
+    background-color: rgba(217, 27, 66, 0.2);
+  }
+  .percent-b {
+    border-left: 4px solid #45c496;
+    background-color: rgba(69, 196, 150, 0.2);
+  }
 </style>
 
 <Card>
@@ -44,11 +61,12 @@
     <h3>{poll.question}</h3>
     <p>Total votes: {totalVotes}</p>
     <div class="answer" on:click={() => handleVote('a', poll.id)}>
-      <div class="percent percent-a" />
+      <!-- dynamic value -->
+      <div class="percent percent-a" style="width: {percentA}%" />
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <div class="answer" on:click={() => handleVote('b', poll.id)}>
-      <div class="percent percent-b" />
+      <div class="percent percent-b" style="width: {percentB}%" />
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
   </div>
